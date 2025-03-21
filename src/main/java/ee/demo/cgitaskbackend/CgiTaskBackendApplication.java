15737bfd2a7ae1741d1703c92da34d718093e55f
datjul <datjul@taltech.ee>
2025-03-21 19:06:33 +0200

    initial commit

---
+++ src/main/java/ee/demo/cgitaskbackend/CgiTaskBackendApplication.java
@@ -0,0 +1,13 @@
+package ee.demo.cgitaskbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgiTaskBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgiTaskBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/demo/cgitaskbackend/CgiTaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.demo.cgitaskbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgiTaskBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
